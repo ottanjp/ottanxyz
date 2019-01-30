93c7c8506d0790db3d88cc6ec1c8e5b598154349
ottan <ottan@ottan.xyz>
2019-01-30 22:16:50 +0900

    サイト内検索の試験的実装

--- themes/ottanxyz/webpack.config.js
+++ themes/ottanxyz/webpack.config.js
@@ -1,18 +1,33 @@
-const path = require('path');
-module.exports = (env, argv) => {
-	const conf = {
-		mode: 'development',
-		devServer: {
-			open: true,
-			contentBase: path.join(__dirname, 'public'),
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+	entry: './assets/js/index.js',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						},
+					},
+				],
+			},
+		],
+	},
+	output: {
+		path: `${__dirname}/static/dist/js`,
+		filename: 'app.js',
+	},
+	plugins: [new LodashModuleReplacementPlugin(), new Dotenv()],
+	resolve: {
+		alias: {
+			vue$: 'vue/dist/vue.esm.js',
 		},
-		entry: { app: './assets/js/index.js' },
-		output: {
-			path: path.join(__dirname, 'static/dist/js'),
-			publicPath: '/js/',
-			filename: '[name].js',
-			libraryTarget: 'umd',
-		},
-	};
-	return conf;
-};
+	},
+	target: 'node',
+}
