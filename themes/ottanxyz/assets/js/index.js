42bb71083d9badf9ca49a91d4a213c8402b52ab6
ottan <ottan@ottan.xyz>
2019-02-02 11:57:13 +0900

    検索結果の改善

--- themes/ottanxyz/assets/js/index.js
+++ themes/ottanxyz/assets/js/index.js
@@ -1,18 +1,15 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
-import { faCalendar, faClock } from '@fortawesome/free-regular-svg-icons'
-import {
-	faAngleLeft,
-	faAngleRight,
-	faFolderOpen,
-	faPencilAlt,
-	faRssSquare,
-	faTag,
-} from '@fortawesome/free-solid-svg-icons'
-import {
-	faFacebook,
-	faTwitter,
-	faGithub,
-} from '@fortawesome/free-brands-svg-icons'
+import { faCalendar } from '@fortawesome/free-regular-svg-icons/faCalendar'
+import { faClock } from '@fortawesome/free-regular-svg-icons/faClock'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons/faAngleLeft'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons/faAngleRight'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons/faFolderOpen'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt'
+import { faRssSquare } from '@fortawesome/free-solid-svg-icons/faRssSquare'
+import { faTag } from '@fortawesome/free-solid-svg-icons/faTag'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
 library.add(
 	faCalendar,
 	faClock,
@@ -37,6 +34,7 @@ new Vue({
 	data: {
 		results: [],
 		query: '',
+		show: false,
 	},
 	created: function() {
 		this.debouncedSearch = _.debounce(this.search, 500)
@@ -55,11 +53,13 @@ new Vue({
 				})
 				.then(results => {
 					this.results = results
+					this.show = true
 				})
 		},
 	},
 	watch: {
 		query: function() {
+			this.show = false
 			this.debouncedSearch()
 		},
 	},
